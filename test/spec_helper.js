5559d2f90e9bb45fc3556bafe752ef523f415606
James Coglan <jcoglan@gmail.com>
2013-02-13 17:49:30 +0000

    Generate fixtures in JS without making assumptions about the host page.

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -2,6 +2,9 @@ JS.ENV.SpecHelper = new JS.Module('SpecHelper', {
   extend: {
     ClassMethods: new JS.Module({
       fixture: function(html) {
+        var fixture = $('#fixture');
+        if (fixture.length === 0) $('body').prepend('<div id="fixture"></div>');
+
         this.before(function() {
           jQuery('#fixture').html(html);
         });
@@ -9,63 +12,8 @@ JS.ENV.SpecHelper = new JS.Module('SpecHelper', {
           jQuery('#fixture').empty();
         });
       },
-      
-      fixtureFrom: function(url, selector) {
-        var doc     = null,
-            element = null,
-            fixture = null;
-        
-        var xhr = jQuery.ajax({url: url});
-        
-        xhr.success(function(response) {
-          doc = jQuery(response);
-          element = doc.find(selector)[0];
-          if (!element) return;
-          
-          fixture = jQuery('<div>').append(element).html();
-          
-          if (window.console) {
-            console.log('Fixture from', url, selector);
-            console.log(fixture);
-          }
-        });
-        
-        this.before(function(resume) {
-          xhr.success(function() {
-            if (fixture) jQuery('#fixture').html(fixture);
-            
-            resume(function() {
-              if (!fixture)
-                throw new Error('Failed to find selector "' + selector + '" at ' + url);
-            });
-          });
-          
-          xhr.error(function(response) {
-            resume(function() {
-              throw new Error('Failed to load fixture from ' + url);
-            });
-          });
-        });
-        
-        this.after(function() {
-          jQuery('#fixture').empty();
-        });
-      },
-      
-      jsonFixtureFrom: function(file) {
-        var path = JS.ENV.ROOT + '/spec/fixtures/' + file,
-            xhr  = jQuery.ajax({url: path});
-        
-        this.before(function(resume) {
-          var self = this;
-          xhr.success(function(response) {
-            self.json = JSON.parse(response);
-            resume();
-          });
-        });
-      }
     }),
-    
+
     included: function(testSuite) {
       testSuite.extend(this.ClassMethods);
     }
